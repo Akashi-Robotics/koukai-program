f22697485c8874c21f66701c4c173152ccf6cff4
Sopuranoaruto <Naka24_0310_IT@icloud.com>
2025-01-11 01:27:46 +0000

    公開講座　プログラム追加

--- main.cpp
+++ main.cpp
@@ -4,20 +4,11 @@
  */
 
 #include "mbed.h"
-#include "platform/mbed_thread.h"
-
-// Blinking rate in milliseconds
-#define BLINKING_RATE_MS   500
-
 
 int main()
 {
-    // Initialise the digital pin LED1 as an output
     DigitalOut led(LED1);
     while (true) {
-        if(int i = 0;i < 4){
-        led = !led;
-        }
-        thread_sleep_for(BLINKING_RATE_MS);
+        
     }
 }
